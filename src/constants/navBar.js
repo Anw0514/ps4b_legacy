1612f7a4ee3cb82a8896f109249ec2d76f051b9c
anw0514 <anw0514@gmail.com>
2019-11-19 13:44:23 -0500

    renamed some files and began adding relationships between components

---
+++ src/constants/navBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <Menu>
+                NavBar
+            </Menu>
+        )
+    }
+}
+
+export default NavBar
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { Component } from 'react';
 import '../css/App.css';
+import NavBar from '../constants/NavBar';
+import Content from './Content';
+import Footer from '../constants/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render () {
+    return (
+      <div className="App">
+        <NavBar />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
