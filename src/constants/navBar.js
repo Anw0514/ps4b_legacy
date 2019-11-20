a7dc6c5e79fd21458459ea8e2a55bc2ae8ab17f9
anw0514 <anw0514@gmail.com>
2019-11-20 14:20:15 -0500

    nav has all the necessary items

--- src/constants/navBar.js
+++ src/constants/navBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Grid, Image, Dropdown } from 'semantic-ui-react'
 
 class NavBar extends Component {
 
@@ -24,10 +24,67 @@ class NavBar extends Component {
 
     render() {
         return (
-            <div className='navbar'>
-                NavBar
-            </div>
-        )
+          <div className="navbar">
+            <Grid>
+              <Grid.Column floated="left" width={6}>
+                <Image src={require("../assets/PSI_Logo_Blue.png")} />
+              </Grid.Column>
+              <Grid.Column
+                floated="right"
+                width={8}
+                verticalAlign="middle"
+                textAlign="right"
+              >
+                <Grid columns={4}>
+                  <Grid.Column>
+                    <Dropdown pointing text="Company">
+                      <Dropdown.Menu>
+                        <Dropdown.Item text="About" />
+                        <Dropdown.Item text="Partners" />
+                        <Dropdown.Item text="Certifications" />
+                        <Dropdown.Item text="Contract Vehicles" />
+                        <Dropdown.Item text="Our Team" />
+                        <Dropdown.Item text="Careers" />
+                        <Dropdown.Item text="Contact" />
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Dropdown pointing text="Services">
+                      <Dropdown.Menu>
+                        <Dropdown.Item text="Security Optimization" />
+                        <Dropdown.Item text="Workplace Modernization" />
+                        <Dropdown.Item text="Network Transformation" />
+                        <Dropdown.Item text="Data Center Consolidation" />
+                        <Dropdown.Item text="e-Learning" />
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Dropdown pointing text="Solutions">
+                      <Dropdown.Menu>
+                        <Dropdown.Item text="Microsoft 365" />
+                        <Dropdown.Item text="Business Apps" />
+                        <Dropdown.Item text="Data and BI" />
+                        <Dropdown.Item text="Apps & Infrastructure" />
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Dropdown pointing text="Products">
+                      <Dropdown.Menu>
+                        <Dropdown.Item text="Exchange Online" />
+                        <Dropdown.Item text="Microsoft 365" />
+                        <Dropdown.Item text="Office 365" />
+                        <Dropdown.Item text="Dynamics 365" />
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Grid.Column>
+                </Grid>
+              </Grid.Column>
+            </Grid>
+          </div>
+        );
     }
 }
 
