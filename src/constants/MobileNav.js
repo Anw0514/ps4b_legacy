7bc6ca5e50d4c4a5281a55abaab6c2b2c689dbce
anw0514 <anw0514@gmail.com>
2019-11-21 14:46:14 -0500

    mobileNav has correct navLinks and large file storage was installed

--- src/constants/MobileNav.js
+++ src/constants/MobileNav.js
@@ -1,9 +1,136 @@
 import React, { Component } from "react";
-import { Icon } from "semantic-ui-react";
+import { Icon, Grid, Accordion } from "semantic-ui-react";
+import { Link as NavLink } from 'react-router-dom'
 
 class MobileNav extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      activeIndex: -1
+    }
+  }
+
+  changeActiveTitle(index) {
+    const activeIndex = this.state.activeIndex === index ? -1 : index
+    this.setState({ activeIndex })
+  }
+
   render() {
-    return <Icon name="list ul" size="large" />
+    const { activeIndex } = this.state
+    return (
+      <Grid.Row>
+        <Accordion className="mobile-accordion">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={() => this.changeActiveTitle(0)}
+            className="nav-dropdown-item"
+          >
+            <Icon name="dropdown" />
+            Company
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <NavLink to="/company" className="mobile-accordion">
+              About
+            </NavLink>
+            <NavLink to="/company" className="mobile-accordion">
+              Partners
+            </NavLink>
+            <NavLink to="/company" className="mobile-accordion">
+              Certifications
+            </NavLink>
+            <NavLink to="/company" className="mobile-accordion">
+              Contract Vehicles
+            </NavLink>
+            <NavLink to="/company" className="mobile-accordion">
+              Our Team
+            </NavLink>
+            <NavLink to="/company" className="mobile-accordion">
+              Careers
+            </NavLink>
+            <NavLink to="/company" className="mobile-accordion">
+              Contact
+            </NavLink>
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={() => this.changeActiveTitle(1)}
+            className="nav-dropdown-item"
+          >
+            <Icon name="dropdown" />
+            Services
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            <NavLink to="/security-optimization" className="mobile-accordion">
+              Security Optimization
+            </NavLink>
+            <NavLink to="/workplace-modernization" className="mobile-accordion">
+              Workplace Modernization
+            </NavLink>
+            <NavLink to="/network-transformation" className="mobile-accordion">
+              Network Transformation
+            </NavLink>
+            <NavLink
+              to="/data-center-consolidation"
+              className="mobile-accordion"
+            >
+              Data Center Consolidation
+            </NavLink>
+            <NavLink to="/e-learning" className="mobile-accordion">
+              E-Learning
+            </NavLink>
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={() => this.changeActiveTitle(2)}
+            className="nav-dropdown-item"
+          >
+            <Icon name="dropdown" />
+            Solutions
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Microsoft 365
+            </NavLink>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Business Apps
+            </NavLink>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Data and BI
+            </NavLink>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Apps & Infrostructure
+            </NavLink>
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 3}
+            index={3}
+            onClick={() => this.changeActiveTitle(3)}
+            className="nav-dropdown-item"
+          >
+            <Icon name="dropdown" />
+            Products
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 3}>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Exchange Online
+            </NavLink>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Microsoft 365
+            </NavLink>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Office 365
+            </NavLink>
+            <NavLink to="/solutions" className="mobile-accordion">
+              Dynamics 365
+            </NavLink>
+          </Accordion.Content>
+        </Accordion>
+      </Grid.Row>
+    );
   }
 }
 
--- src/constants/navBar.js
+++ src/constants/navBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Menu, Grid, Image, Dropdown } from 'semantic-ui-react'
+import { Menu, Grid, Image, Dropdown, Icon } from 'semantic-ui-react'
 import { Link as NavLink } from 'react-router-dom'
 import MobileNav from './MobileNav'
 
@@ -35,11 +35,12 @@ class NavBar extends Component {
     }
 
     render() {
-        const { mobile, dropdown } = this.state
+      const { mobile, dropdown } = this.state
 
-        return (
-          <div className="navbar">
-            <Grid>
+      return (
+        <div className="navbar">
+          <Grid>
+            <Grid.Row>
               <Grid.Column
                 floated="left"
                 width={6}
@@ -51,149 +52,169 @@ class NavBar extends Component {
                   <Image src={require("../assets/PSI_Logo_Blue.png")} />
                 </NavLink>
               </Grid.Column>
-              { mobile ?
-              <Grid.Column 
-                width={10} 
-                floated='right' 
-                textAlign='right'
-                verticalAlign='middle'
-              >
-                  <MobileNav />
-              </Grid.Column> :
-              <Grid.Column
-                floated="right"
-                width={10}
-                tablet={10}
-                largeScreen={8}
-                widescreen={4}
-                verticalAlign="middle"
-                textAlign="right"
-              >
-                <Grid columns={4}>
-                  <Grid.Column onMouseEnter={() => this.dropdown("Company")} onMouseLeave={() => this.close()}>
-                    <NavLink to="/company">
-                      <Dropdown open={dropdown === "Company"} text="Company" >
+              {mobile ? (
+                <Grid.Column
+                  width={10}
+                  floated="right"
+                  textAlign="right"
+                  verticalAlign="middle"
+                >
+                  <Icon className="mobile-dropdown" name="list ul" size="large" />
+                </Grid.Column>
+              ) : (
+                <Grid.Column
+                  floated="right"
+                  width={10}
+                  tablet={10}
+                  largeScreen={8}
+                  widescreen={4}
+                  verticalAlign="middle"
+                  textAlign="right"
+                >
+                  <Grid columns={4}>
+                    <Grid.Column
+                      onMouseEnter={() => this.dropdown("Company")}
+                      onMouseLeave={() => this.close()}
+                    >
+                      <NavLink to="/company">
+                        <Dropdown open={dropdown === "Company"} text="Company">
+                          <Dropdown.Menu>
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="About"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Partners"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Certifications"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Contract Vehicles"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Our Team"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Careers"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Contact"
+                            />
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </NavLink>
+                    </Grid.Column>
+                    <Grid.Column
+                      onMouseEnter={() => this.dropdown("Services")}
+                      onMouseLeave={() => this.close()}
+                    >
+                      <Dropdown open={dropdown === "Services"} text="Services">
                         <Dropdown.Menu>
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="About"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Partners"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Certifications"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Contract Vehicles"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Our Team"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Careers"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Contact"
-                          />
+                          <NavLink to="/security-optimization">
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Security Optimization"
+                            />
+                          </NavLink>
+                          <NavLink to="/workplace-modernization">
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Workplace Modernization"
+                            />
+                          </NavLink>
+                          <NavLink to="/network-transformation">
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Network Transformation"
+                            />
+                          </NavLink>
+                          <NavLink to="/data-center-consolidation">
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Data Center Consolidation"
+                            />
+                          </NavLink>
+                          <NavLink to="/e-learning">
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="e-Learning"
+                            />
+                          </NavLink>
                         </Dropdown.Menu>
                       </Dropdown>
-                    </NavLink>
-                  </Grid.Column>
-                  <Grid.Column onMouseEnter={() => this.dropdown("Services")} onMouseLeave={() => this.close()}>
-                    <Dropdown open={dropdown === "Services"} text="Services">
-                      <Dropdown.Menu>
-                        <NavLink to="/security-optimization">
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Security Optimization"
-                          />
-                        </NavLink>
-                        <NavLink to="/workplace-modernization">
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Workplace Modernization"
-                          />
-                        </NavLink>
-                        <NavLink to="/network-transformation">
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Network Transformation"
-                          />
-                        </NavLink>
-                        <NavLink to="/data-center-consolidation">
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Data Center Consolidation"
-                          />
-                        </NavLink>
-                        <NavLink to="/e-learning">
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="e-Learning"
-                          />
-                        </NavLink>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Grid.Column>
-                  <Grid.Column onMouseEnter={() => this.dropdown("Solutions")} onMouseLeave={() => this.close()}>
-                    <NavLink to="/solutions">
-                      <Dropdown open={dropdown === "Solutions"} text="Solutions">
+                    </Grid.Column>
+                    <Grid.Column
+                      onMouseEnter={() => this.dropdown("Solutions")}
+                      onMouseLeave={() => this.close()}
+                    >
+                      <NavLink to="/solutions">
+                        <Dropdown
+                          open={dropdown === "Solutions"}
+                          text="Solutions"
+                        >
+                          <Dropdown.Menu>
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Microsoft 365"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Business Apps"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Data and BI"
+                            />
+                            <Dropdown.Item
+                              className="nav-dropdown-item"
+                              text="Apps & Infrastructure"
+                            />
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </NavLink>
+                    </Grid.Column>
+                    <Grid.Column
+                      onMouseEnter={() => this.dropdown("Products")}
+                      onMouseLeave={() => this.close()}
+                    >
+                      <Dropdown open={dropdown === "Products"} text="Products">
                         <Dropdown.Menu>
+                          <Dropdown.Item
+                            className="nav-dropdown-item"
+                            text="Exchange Online"
+                          />
                           <Dropdown.Item
                             className="nav-dropdown-item"
                             text="Microsoft 365"
                           />
                           <Dropdown.Item
                             className="nav-dropdown-item"
-                            text="Business Apps"
+                            text="Office 365"
                           />
                           <Dropdown.Item
                             className="nav-dropdown-item"
-                            text="Data and BI"
-                          />
-                          <Dropdown.Item
-                            className="nav-dropdown-item"
-                            text="Apps & Infrastructure"
+                            text="Dynamics 365"
                           />
                         </Dropdown.Menu>
                       </Dropdown>
-                    </NavLink>
-                  </Grid.Column>
-                  <Grid.Column onMouseEnter={() => this.dropdown("Products")} onMouseLeave={() => this.close()}>
-                    <Dropdown open={dropdown === "Products"} text="Products" >
-                      <Dropdown.Menu>
-                        <Dropdown.Item
-                          className="nav-dropdown-item"
-                          text="Exchange Online"
-                        />
-                        <Dropdown.Item
-                          className="nav-dropdown-item"
-                          text="Microsoft 365"
-                        />
-                        <Dropdown.Item
-                          className="nav-dropdown-item"
-                          text="Office 365"
-                        />
-                        <Dropdown.Item
-                          className="nav-dropdown-item"
-                          text="Dynamics 365"
-                        />
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Grid.Column>
-                </Grid>
-              </Grid.Column>
-            }
-            </Grid>
-          </div>
-        );
+                    </Grid.Column>
+                  </Grid>
+                </Grid.Column>
+              )}
+            </Grid.Row>
+            { mobile ?
+              <MobileNav />
+            : null }
+          </Grid>
+        </div>
+      );
     }
 }
 
