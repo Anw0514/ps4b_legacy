56f2f790b347ea01a2daa21007855dcc6668e40e
anw0514 <anw0514@gmail.com>
2019-11-22 18:32:54 -0500

    ImgDiv Component almost complete

--- src/pages/services/NetworkTransformation.js
+++ src/pages/services/NetworkTransformation.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import ImgDiv from '../../reusable/ImgDiv'
 
 class NetworkTransformation extends Component {
     render() {
         return (
-            <div>
-                NetworkTransformation
-            </div>
-        )
+          <div className="bg-light-grey">
+            NetworkTransformation
+            <ImgDiv
+              image={require("../../assets/spongebob_computer.jpg")}
+              paragraphs={["fuck this", "okay how about no"]}
+              title="Here is how PSI can help your network transformation:"
+              right={true}
+            />
+            <ImgDiv
+              image={require("../../assets/spongebob_computer.jpg")}
+              list={[
+                "Assess your network’s security to ensure the necessary controls are in place",
+                "Network mapping",
+                "Assess your organization’s readiness for alternative network architectures",
+                "Design and deploy the most efficient architecture that is customized for your specific requirements and priorities",
+                "Provide you with a realistic plan to evolve your network based upon current and future standards and recommendations while considering logistical constraints",
+                "Provide network infrastructure managed services"
+              ]}
+              title="Here is how PSI can help your network transformation:"
+            />
+          </div>
+        );
     }
 }
 
---
+++ src/reusable/ImgDiv.js
@@ -0,0 +1,64 @@
+import React, { Component, Fragment } from "react";
+import { Image, Grid, List } from "semantic-ui-react";
+
+class ImgDiv extends Component {
+
+  content(list, paragraphs) {
+    if (list) {
+      return (
+        <List bulleted>
+          {list.map(item => {
+            return <List.Item className='imgdiv-item'>{item}</List.Item>
+          })}
+        </List>
+      )
+    } else {
+      return paragraphs.map(pgrph => {
+        return <p>{pgrph}</p>;
+      });
+    }
+  }
+
+  render() {
+    const { image, right, list, title, paragraphs } = this.props
+    return (
+      <div className="imgdiv regular-section bg-white rounded-corners">
+        <Grid stackable>
+          {right ? (
+            <Fragment>
+              <Grid.Column width={10}>
+                <div className="imgdiv-text">
+                  <h2>{title}</h2>
+                  {this.content(list, paragraphs)}
+                </div>
+              </Grid.Column>
+              <Grid.Column style={{minHeight: '400px'}} className="noPadding" width={6}>
+                <div
+                  style={{ backgroundImage: `url(${image})` }}
+                  className="imgdiv-img"
+                ></div>
+              </Grid.Column>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Grid.Column style={{minHeight: '400px'}} className="noPadding" width={6}>
+                <div
+                  style={{ backgroundImage: `url(${image})` }}
+                  className="imgdiv-img"
+                ></div>
+              </Grid.Column>
+              <Grid.Column width={10}>
+                <div className="imgdiv-text">
+                  <h2>{title}</h2>
+                  {this.content(list, paragraphs)}
+                </div>
+              </Grid.Column>
+            </Fragment>
+          )}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default ImgDiv;
