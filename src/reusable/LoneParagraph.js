cd244ad716069a87d1e163c3d1f30703a9135a40
anw0514 <anw0514@gmail.com>
2019-11-25 12:12:22 -0500

    created basic component files for SubHeader and LoneParagraph

---
+++ src/reusable/LoneParagraph.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class LoneParagraph extends Component {
+  render() {
+    return <div>LoneParagraph</div>;
+  }
+}
+
+export default LoneParagraph;
---
+++ src/reusable/SubHeader.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SubHeader extends Component {
+  render() {
+    return <div>SubHeader</div>;
+  }
+}
+
+export default SubHeader;
