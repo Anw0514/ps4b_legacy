fb72be9214407710b16c1beab49c34e1e2e79db4
anw0514 <anw0514@gmail.com>
2019-11-26 17:00:25 -0500

    exchange online content

--- src/pages/products/ExchangeOnline.js
+++ src/pages/products/ExchangeOnline.js
@@ -1,51 +1,68 @@
 import React, { Component, Fragment } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Card, Image } from 'semantic-ui-react'
 import IconParagraph from '../../reusable/IconParagraph'
 import SubHeader from '../../reusable/SubHeader'
 
 class ExchangeOnline extends Component {
     render() {
+        const plans = [{ id: "1", content: "50 GB of inbox storage and can send messages up to 150 MB, inbox management, sharing of calendars", price: "4"},
+            { id: "2", content: "100 GB of inbox storage and can send messages up to 150 MB, inbox management, sharing of calendars Data Loss Prevention (DLP), voicemail", price: "8"}]
         return (
-            <Fragment>
-                <div className="bg-white narrow-section">
-                    <div className="subheader">
-                        <h3 className="fs-3">Microsoft Exchange Online</h3>
-                        <p>
-                            Fully hosted email plans complete with online calendars and 
-                            contacts migration and support included!
-                        </p>
-                        <hr />
-                    </div>
-                    <h4 className="fs-2 subheader">WE MAKE EXCHANGE ONLINE ADOPTION CHAOS FREE</h4>
-                    <Grid stackable columns={3} className="bg-white m-top-3">
-                        <Grid.Column>
-                            <IconParagraph
-                                title="Expert and dedicated migration from any email platform."
-                                iconName="Tactic"
-                            /> 
-                        </Grid.Column>
-                        <Grid.Column>
-                            <IconParagraph
-                                title="30-day money back guarantee. No setup fees or yearly contracts."
-                                iconName="Money"
-                            />
-                        </Grid.Column>
-                        <Grid.Column>
-                            <IconParagraph
-                                title="Free Support."
-                                iconName="Support"
-                            />
-                        </Grid.Column>
-                    </Grid>
-                </div>
-                <div className="bg-light-grey page-content">
-                    <div className="narrow-section top-pad-1">
-                        <SubHeader title="All the Exchange Online Plans for Your Business" />
-                        <Grid relaxed='very' columns={2}>
-                        </Grid>
-                    </div>
-                </div>
-            </Fragment>
+          <Fragment>
+            <div className="bg-white narrow-section">
+              <div className="subheader">
+                <h3 className="fs-3">Microsoft Exchange Online</h3>
+                  <p>
+                    Fully hosted email plans complete with online calendars and 
+                    contacts migration and support included!
+                  </p>
+                  <hr />
+              </div>
+              <h4 className="fs-2 subheader">WE MAKE EXCHANGE ONLINE ADOPTION CHAOS FREE</h4>
+              <Grid stackable columns={3} className="bg-white m-top-3">
+                <Grid.Column>
+                  <IconParagraph
+                    title="Expert and dedicated migration from any email platform."
+                    iconName="Tactic"
+                  /> 
+                </Grid.Column>
+                <Grid.Column>
+                  <IconParagraph
+                    title="30-day money back guarantee. No setup fees or yearly contracts."
+                    iconName="Money"
+                  />
+                </Grid.Column>
+                <Grid.Column>
+                  <IconParagraph
+                    title="Free Support."
+                    iconName="Support"
+                  />
+                </Grid.Column>
+              </Grid>
+            </div>
+            <div className="bg-light-grey page-content">
+              <div className="narrow-section top-pad-1">
+                <SubHeader title="Exchange Online Plans for Your Business" />
+                <Grid relaxed='very' columns={2}>
+                  { plans.map(plan => {
+                    return (
+                      <Grid.Column>
+                        <div className='rounded-corners bg-white fluid' style={{minHeight: "100%", overflow: "hidden"}}>
+                          <div className='exchange-online-img' />
+                          <div className='padded'>
+                            <h4 className="fs-2">Exchange Online Plan {plan.id}</h4>
+                            <p>{plan.content}</p>
+                            <h3 className="fs-3">${plan.price}.00</h3>
+                            <p className="fs-0">per user/per month</p>
+                          </div>
+                        </div>
+                      </Grid.Column>
+                    )
+                  }) }
+                </Grid>
+              </div>
+            </div>
+          </Fragment>
         )
     }
 }
---
+++ src/reusable/ProductPlans.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProductPlans extends Component {
+    render() {
+        return (
+            <div>
+                ProductPlans
+            </div>
+        )
+    }
+}
+
+export default ProductPlans
