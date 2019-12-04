717bd274d7845a2bf1ef003e395655d0a2a5cad4
anw0514 <anw0514@gmail.com>
2019-12-04 15:12:12 -0500

    footer looks good on wide screen but bad on laptop

--- src/constants/footer.js
+++ src/constants/footer.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react'
-import { Grid, Button } from 'semantic-ui-react'
+import { Grid, Button, Image, Icon } from 'semantic-ui-react'
 
 class Footer extends Component {
     render() {
         return (
-            <div className="bg-blue fluid hella-padded">
-                <Grid stackable centered>
-                    <Grid.Column verticalAlign="middle" className="text-center" width={11}>
-                        <h1 className="white fs-3" >How can we help? Schedule a free consultation!</h1>
+            <div className="bg-blue-2 fluid pretty-padded">
+                <Grid doubling centered columns={5} className='regular-width'>
+                    <Grid.Column verticalAlign="middle" textAlign='right'>
+                        <Image src={require('../assets/PSI_Logo_Dark.png')} floated='right' />
                     </Grid.Column>
-                    <Grid.Column verticalAlign="middle" width={5}>
-                        <Button circular inverted size="big">Contact Us</Button>
+                    <Grid.Column verticalAlign="middle" className="text-left">
+                        <h2 className="white fs-2" >Headquarters</h2>
+                        <p className="white fs-0">
+                            20 F Street NW, Suite 700 <br />
+                            Washington, DC 20001 <br />
+                            USA
+                        </p>
+                    </Grid.Column>
+                    <Grid.Column verticalAlign="middle" textAlign='left'>
+                        <h2 className="white fs-1" >Privacy Policy</h2>
+                        <h2 className="white fs-1" >Other Locations</h2>
+                        <p className="white indented-s fs-0">
+                            McLean, Virginia<br />
+                            West Chester Pennsylvania<br />
+                            Phoenix, Arizona
+                        </p>
+                    </Grid.Column>
+                    <Grid.Column verticalAlign="middle" textAlign='left'>
+                        <h2 className="white fs-1" >Give Us a Call</h2>
+                        <p className="white">+1 (202)239-6247</p>
+                        <h2 className="white fs-1" >Email Us</h2>
+                        <p className="white">info@ps4b.com</p>
+                    </Grid.Column>
+                    <Grid.Column verticalAlign="middle" textAlign='left'>
+                        <Grid columns={2} relaxed>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    <Icon name='linkedin alternate' size='big' />
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Icon name='twitter square' size='big' />
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    <Icon name='facebook square' size='big' />
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Icon name='youtube' size='big' />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
                     </Grid.Column>
                 </Grid>
             </div>
