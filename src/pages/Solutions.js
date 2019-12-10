c8ed5efac32d392c716c82c4c35e9d7d841ac160
anw0514 <anw0514@gmail.com>
2019-12-10 16:28:21 -0500

    added lone paragraph to apps and infrastructure

--- src/pages/Solutions.js
+++ src/pages/Solutions.js
@@ -102,6 +102,33 @@ class Solutions extends Component {
                     </Grid>
                 </div>
                 <div className="filler" />
+                <div className="bg-light-grey">
+                    <SubHeader paddingTop marginBottom title="Apps and Infrastructure" />
+                    <div className="narrow-width bg-white lone-paragraph rounded-corners">
+                        <h5>How do we transform your infrastructure?</h5>
+                        <p className="text-left drop-cap">
+                            Practical Solutions, Inc. (PSI) is a leader in helping businesses 
+                            transform and grow by optimizing their IT infrastructure. PSI 
+                            understands that the key to transformation is an IT foundation 
+                            that is stable, robust, and secure. As a Microsoft Gold partner, 
+                            we rely heavily on Microsoft applications and infrastructure to 
+                            ensure our clients have the right foundation to enable their 
+                            business transformation.
+                        </p>
+                        <p className="text-left">
+                            The PSI team can design, implement, and manage a complete 
+                            end-to-end infrastructure optimization solution — transforming the 
+                            IT infrastructure from a cost center into a strategic asset. We 
+                            work closely with clients to develop a balanced solution that takes 
+                            advantage of infrastructure optimization guidance from Microsoft, 
+                            in order to help our clients create an efficient and sustainable IT 
+                            environment. PSI can help clients assess basic IT capabilities within 
+                            their infrastructure, and then execute a multi-tier improvement plan.
+                        </p>
+                    </div>
+                    <div className="filler" />
+
+                </div>
             </div>
         )
     }
