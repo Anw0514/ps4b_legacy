c6a26739122ed1ba6d0c71047d86ff3b26dfb190
anw0514 <anw0514@gmail.com>
2019-12-16 13:41:42 -0500

    using video instead of gif now

--- src/constants/HomeHeader.js
+++ src/constants/HomeHeader.js
@@ -3,17 +3,25 @@ import React, { Component } from "react";
 class HomeHeader extends Component {
   render() {
     return (
-        <div className="pageTitle home">
-        <div className="text-left">
-            <h1 className="white">Practical Solutions, Inc.</h1>
-            <h4 className="white">
+      <div className="pageTitle">
+        <div className="video-holder">
+          <video preload="preload" id="video" autoPlay="autoplay" loop="loop">
+            <source
+              src={require("../assets/backgrounds/background_video.mp4")}
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <div className="text-left home">
+          <h1 className="white">Practical Solutions, Inc.</h1>
+          <h4 className="white">
             Over 20 years of experience in helping clients
             <br />
             Turn Chaos to Order
-            </h4>
+          </h4>
         </div>
-        </div>
-    )
+      </div>
+    );
   }
 }
 
