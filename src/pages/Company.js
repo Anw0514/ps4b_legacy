7f9d0d6696e8b0ad3fd094adc5c190c83cc1acfd
anw0514 <anw0514@gmail.com>
2020-01-15 16:07:05 -0500

    finished exporting sections in company

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -9,6 +9,8 @@ import Careers from './sections/Careers'
 import ContractVehicles from './sections/ContractVehicles';
 import Certifications from './sections/Certifications';
 import OurTeam from './sections/OurTeam';
+import Partners from './sections/Partners';
+import About from './sections/About';
 
 
 class Company extends Component {
@@ -27,6 +29,7 @@ class Company extends Component {
   }
 
   componentDidMount() {
+    // very hacky lol sorry
     this.componentDidUpdate();
   }
 
@@ -63,39 +66,6 @@ class Company extends Component {
     });
   };
 
-  buildPartner(partner) {
-    return (
-      <Grid centered columns={2}>
-        <Grid.Row>
-          <Grid.Column verticalAlign="middle">
-            <Image centered src={require(`../assets/about/${partner[0]}`)} />
-          </Grid.Column>
-          <Grid.Column verticalAlign="middle">
-            {partner[1] ? (
-              <Image centered src={require(`../assets/about/${partner[1]}`)} />
-            ) : (
-              <div></div>
-            )}
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column verticalAlign="middle">
-            {partner[2] ? (
-              <Image centered src={require(`../assets/about/${partner[2]}`)} />
-            ) : (
-              <div></div>
-            )}
-          </Grid.Column>
-          {partner[3] ? (
-            <Grid.Column verticalAlign="middle">
-              <Image centered src={require(`../assets/about/${partner[3]}`)} />
-            </Grid.Column>
-          ) : null}
-        </Grid.Row>
-      </Grid>
-    );
-  }
-
   render() {
     const { person, open } = this.state;
 
@@ -103,42 +73,8 @@ class Company extends Component {
 
     return (
       <Fragment>
-        <div id="about" className="bg-light-grey">
-          <SubHeader title="Introduction" marginBottom paddingTop />
-          <LoneParagraph
-            text={[
-              "Practical Solutions Inc (PSI) was founded in 1997 by Mr. JayHajeer, who led the development of PSI into a modernhigh-performance global organization that embodies his vision: “We Deliver Practical Solutions”"
-            ]}
-            list={introList}
-          />
-          <SubHeader
-            title="Our Methodology and Differentiators"
-            marginBottom
-            marginTop
-          />
-          <ImgDiv
-            image={require("../assets/about/about.jpg")}
-            list={[
-              "Many of our services and products are paired with eLearning so clients get a faster ROI by having a more prepared team following implementation of new solution.",
-              "A lot of companies will offer text book solutions to their customers, we dig in, understand the company, its unique characteristics and provide a solution that is practical and unique.",
-              "Our ongoing Reputation and relationships are more important to us than a quick profit. Our solutions are designed to be sustainable within your organization beyond our relationship.",
-              "PSI has a seasoned team with decades of experience, click here to view our team.",
-              "100% US Based team serving global clients."
-            ]}
-          />
-          <div className="filler" />
-        </div>
-        <div id="partners" className="bg-white">
-          <SubHeader title="Our Partners" paddingTop marginBottom />
-          <Carousel
-            arrows
-            width="hella-narrow-width"
-            slides={partners.map(partner => this.buildPartner(partner))}
-            show={1}
-            scroll={1}
-          />
-          <div className="filler" />
-        </div>
+        <About introList={introList} />
+        <Partners partners={partners} />
         <div className="bg-light-grey">
           <Certifications />
           <ContractVehicles contractRows={contractRows} />
--- src/pages/sections/About.js
+++ src/pages/sections/About.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import SubHeader from '../../reusable/SubHeader'
+import ImgDiv from '../../reusable/ImgDiv'
+import LoneParagraph from '../../reusable/LoneParagraph'
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        blep
-      </div>
-    )
+			<div id='about' className='bg-light-grey'>
+				<SubHeader title='Introduction' marginBottom paddingTop />
+				<LoneParagraph
+					text={[
+						'Practical Solutions Inc (PSI) was founded in 1997 by Mr. JayHajeer, who led the development of PSI into a modernhigh-performance global organization that embodies his vision: “We Deliver Practical Solutions”',
+					]}
+					list={this.props.introList}
+				/>
+				<SubHeader title='Our Methodology and Differentiators' marginBottom marginTop />
+				<ImgDiv
+					image={require('../../assets/about/about.jpg')}
+					list={[
+						'Many of our services and products are paired with eLearning so clients get a faster ROI by having a more prepared team following implementation of new solution.',
+						'A lot of companies will offer text book solutions to their customers, we dig in, understand the company, its unique characteristics and provide a solution that is practical and unique.',
+						'Our ongoing Reputation and relationships are more important to us than a quick profit. Our solutions are designed to be sustainable within your organization beyond our relationship.',
+						'PSI has a seasoned team with decades of experience, click here to view our team.',
+						'100% US Based team serving global clients.',
+					]}
+				/>
+				<div className='filler' />
+			</div>
+		);
   }
 }
