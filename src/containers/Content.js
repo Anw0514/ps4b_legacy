a40837baa31fc64a4c6de101c2c69c8a79f29231
anw0514 <anw0514@gmail.com>
2020-01-28 11:49:09 -0500

    privacy policy is its own page

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -18,6 +18,7 @@ import SecurityOptimization from "../pages/services/SecurityOptimization";
 import Services from "../pages/services/Services";
 import ManagedServices from "../pages/services/ManagedServices";
 import WorkplaceModernization from "../pages/services/WorkplaceModernization";
+import PrivacyPolicy from '../constants/PrivacyPolicy';
 
 
 class Content extends Component {
@@ -31,63 +32,66 @@ class Content extends Component {
 
     render() {
         return (
-          <div className="content-page">
-            <Switch>
-              <Route exact path="/">
-                <Home open={this.props.openModal} />
-              </Route>
-              <Route exact path="/company">
-                <Company />
-              </Route>
-              <Route exact path="/contact">
-                <Contact
-                  subject={this.props.subject}
-                  changeSubject={this.props.changeSubject}
-                  submitForm={this.props.submitForm}
-                />
-              </Route>
-              <Route exact path="/solutions">
-                <Solutions />
-              </Route>
-              <Route exact path="/products">
-                <Products />
-              </Route>
-              <Route exact path="/exchange-online">
-                <ExchangeOnline />
-              </Route>
-              <Route exact path="/dynamics365">
-                <Dynamics365 />
-              </Route>
-              <Route exact path="/microsoft365">
-                <Microsoft365 />
-              </Route>
-              <Route exact path="/office365">
-                <Office365 />
-              </Route>
-              <Route exact path="/services">
-                <Services />
-              </Route>
-              <Route exact path="/managed-services">
-                <ManagedServices />
-              </Route>
-              <Route exact path="/data-center-consolidation">
-                <DataCenterConsolidation />
-              </Route>
-              <Route exact path="/e-learning">
-                <ELearning />
-              </Route>
-              <Route exact path="/network-transformation">
-                <NetworkTransformation />
-              </Route>
-              <Route exact path="/security-optimization">
-                <SecurityOptimization />
-              </Route>
-              <Route exact path="/workplace-modernization">
-                <WorkplaceModernization />
-              </Route>
-            </Switch>
-          </div>
-        );
+					<div className='content-page'>
+						<Switch>
+							<Route exact path='/'>
+								<Home open={this.props.openModal} />
+							</Route>
+							<Route exact path='/company'>
+								<Company />
+							</Route>
+							<Route exact path='/contact'>
+								<Contact
+									subject={this.props.subject}
+									changeSubject={this.props.changeSubject}
+									submitForm={this.props.submitForm}
+								/>
+							</Route>
+							<Route exact path='/solutions'>
+								<Solutions />
+							</Route>
+							<Route exact path='/products'>
+								<Products />
+							</Route>
+							<Route exact path='/exchange-online'>
+								<ExchangeOnline />
+							</Route>
+							<Route exact path='/dynamics365'>
+								<Dynamics365 />
+							</Route>
+							<Route exact path='/microsoft365'>
+								<Microsoft365 />
+							</Route>
+							<Route exact path='/office365'>
+								<Office365 />
+							</Route>
+							<Route exact path='/services'>
+								<Services />
+							</Route>
+							<Route exact path='/managed-services'>
+								<ManagedServices />
+							</Route>
+							<Route exact path='/data-center-consolidation'>
+								<DataCenterConsolidation />
+							</Route>
+							<Route exact path='/e-learning'>
+								<ELearning />
+							</Route>
+							<Route exact path='/network-transformation'>
+								<NetworkTransformation />
+							</Route>
+							<Route exact path='/security-optimization'>
+								<SecurityOptimization />
+							</Route>
+							<Route exact path='/workplace-modernization'>
+								<WorkplaceModernization />
+							</Route>
+							<Route exact path='/privacy'>
+								<PrivacyPolicy />
+							</Route>
+						</Switch>
+					</div>
+				);
     }
 }
 
