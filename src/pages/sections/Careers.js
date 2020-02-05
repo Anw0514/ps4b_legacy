c66afdc1704b39c643d0ec253243d19dc2fc7831
Shujaat Azim <shujaat.azim@gmail.com>
2020-02-05 15:44:10 -0500

    Fixed a bunch of small but important grammatical and idiomatic errors in Data, About, Careers, and Certifications components. Also testing my ability and authorization to push deployment-ready changes.

--- src/pages/sections/Careers.js
+++ src/pages/sections/Careers.js
@@ -10,7 +10,7 @@ export default class Careers extends Component {
 				<ImgDiv
 					image={require('../../assets/about/careers.jpg')}
 					paragraphs={[
-						'Practical Solutions, Inc. (PSI) is looking for highly motivated and highly skilled individuals who believe in our mission “Turn Chaos into Order” and are ready for exciting opportunities with a growing company. The PSI team hold themselves to very high standards, both professionally and ethically. All our employees are expected to expand their existing skillset and develop new skills in different areas to support our mission. Each employee prides themselves on their commitment to the company, ability to add value and contribute to our growth and success.',
+						'Practical Solutions, Inc. (PSI) is looking for highly motivated, highly skilled individuals who believe in our mission of turning chaos into order, and who are ready for exciting opportunities with a growing company. The PSI team members hold themselves to very high professional and ethical standards. All of our employees are expected to expand their existing skillset and to develop new skills in different areas to support our mission. Each employee takes pride in their commitment to the company, their ability to add value, and their contributions to our growth and success.',
 						'PSI is eagerly seeking talented, creative, and flexible problem solvers to join our growing team. We continue to expand our internal operations, consulting, and staffing services divisions. PSI is an Equal Opportunity Employer and we are always accepting resumes and actively looking for partners. If you are interested in opportunities at PSI, send in your resume and salary requirements to jobs@ps4b.com.',
 					]}
 				/>
